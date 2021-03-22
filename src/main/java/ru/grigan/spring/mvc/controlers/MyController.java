088a53d0539843684bae42f53b1250194c071003
grigan-Uragan <prischepny@mail.ru>
2021-03-23 00:12:26 +0300

    first controller

---
+++ src/main/java/ru/grigan/spring/mvc/controlers/MyController.java
@@ -0,0 +1,13 @@
+package ru.grigan.spring.mvc.controlers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/")
+    public String showFirstView() {
+        return "first-view";
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/first-view.jsp
@@ -0,0 +1,10 @@
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>First view</title>
+</head>
+<body>
+<h2>Hello Grigan Uragan</h2>
+</body>
+</html>
